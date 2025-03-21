2e0b712daa45440c6de079e2f409563c137cb8db
thongminh <thong.dangnguyenminh@vti.com.vn>
2025-03-21 18:32:45 +0700

    new changesssss

---
+++ src/main/java/com/example/musicstore/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.musicstore.config;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                DispatcherType.INCLUDE) .permitAll()
+                        .requestMatchers( "/","login", "/client/**", "/css/**", "/js/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .failureUrl("/login?error")
+                        .permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/home", true)
+                        .failureUrl("/login?error=true")
+                )
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/home")
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                );
+        return http.build();
+    }
+}
+
